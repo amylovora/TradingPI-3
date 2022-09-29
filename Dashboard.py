01625b8ce3e395e3123b4be4a5a50b10459836b1
amylovora <mvresyes1@uc.cl>
2022-09-28 22:37:20 -0300

    last version

--- Dashboard.py
+++ Dashboard.py
@@ -1,71 +1,121 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
 
+import os
 import streamlit as st
+from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
+import cufflinks as cf
+import plotly.graph_objects as px
+from plotly.subplots import make_subplots
+import time
 
-### Dibujar tabla
+from DataTrading import *
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-
-st.dataframe(dataframe.style.highlight_max(axis=0))
-
-
-### Dibujar gráfico
-
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
-
-
-### Dibujar slide 
-
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
-
-
-### No entiendo qué hace
-
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-    })
-
-option = st.selectbox(
-    'Which number do you like best?',
-     df['first column'])
-
-'You selected: ', option
-
-
-### Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
+st.set_page_config(
+    page_title="PI-3 CryptoCoin Easy",
+    page_icon="✅",
+    layout="wide",
 )
 
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
+st.title("CryptoCoin Info")
+
+### Seleccion crypto:
+add_selectbox = st.selectbox(
+    'Criptomoneda',
+    ('BTC', 'ETH', 'BNB', 'XRP', 'DOGE', 'SHIB', 'ATOM', 'LUNC', 'FTT', 'SOL')
 )
 
 
+### Seleccion intervalo tiempo:
 
-left_column, right_column = st.columns(2)
-# You can use a column just like st.sidebar:
-left_column.button('Press me!')
+add_selecttime = st.sidebar.selectbox(
+    'Intervalo de tiempo',
+    ('1 minuto', '5 minutos', '15 minutos', '1 hora', '1 dia', '1 semana', '1 mes')
+)
 
-# Or even better, call Streamlit functions inside a "with" block:
-with right_column:
-    chosen = st.radio(
-        'Sorting hat',
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-    st.write(f"You are in {chosen} house!")
+if add_selecttime == '1 minuto':
+    interval = 60
+elif add_selecttime == '5 minutos':
+    interval = 60*5
+elif add_selecttime == '15 minutos':
+    interval = 60*15
+elif add_selecttime == '1 hora':
+    interval = 60*60
+elif add_selecttime == '1 dia':
+    interval = 60*60*24
+elif add_selecttime == '1 semana':
+    interval = 60*60*24*7
+elif add_selecttime == '1 mes':
+    interval = 60*60*24*30
+
+### Calendario
+yesterday = datetime.now() - timedelta(1)
+
+d = st.sidebar.date_input(
+    "Fecha inicio",
+    yesterday)
+
+fecha = datetime.strptime(str(d), '%Y-%m-%d')
+y = fecha.year
+dia = fecha.day
+mes = fecha.month
+
+### USD Observado hoy
+
+dolarData = getUSD()
+dolarObservado = pd.DataFrame(dolarData['serie'])
+dolarObservado = dolarObservado['valor']
+print(dolarObservado.to_string(index=False))
+
+# create three columns
+kpi1, kpi2, kpi3 = st.columns(3)
+
+# fill in those three columns with respective metrics or KPIs
+kpi1.metric(
+    label="Fecha de hoy",
+    value=str(datetime.utcnow().date())
+)
+
+kpi2.metric(
+    label="Cypto a USD",
+    value=str(datetime.utcnow().date())
+)
+
+kpi3.metric(
+    label="USD a CLP Observado",
+    value=dolarObservado.to_string(index=False)
+)
+
+### Data
+crypto = getData(add_selectbox, str(interval), y, mes, dia)
+crypto['average_high'] = crypto['high'].rolling(5).mean()
+crypto['average_low'] = crypto['low'].rolling(5).mean()
+
+
+
+### Plot: variabilidad cripto y volumen
+#fig = px.Figure()
+titulo_sub = ("Variación " + add_selectbox)
+fig = make_subplots(rows=2, cols=1, shared_xaxes=True, 
+               vertical_spacing=0.03, subplot_titles=(add_selectbox, 'Volume'), 
+               row_width=[0.4, 1.2])
+
+fig.add_trace(px.Candlestick(x=crypto["date"], open=crypto["open"], high=crypto["high"],
+                low=crypto["low"], close=crypto["close"], name=titulo_sub),
+                row=1, col=1)
+
+fig.add_trace(px.Scatter(x=crypto["date"], y=crypto["average_high"], name= 'Media movil alta', line=dict(color='blue', width=2)), row=1, col=1)
+fig.add_trace(px.Scatter(x=crypto["date"], y=crypto["average_low"], name= 'Media movil baja', line=dict(color='orange', width=2)), row=1, col=1)
+
+
+# Bar trace for volumes on 2nd row without legend
+fig.add_trace(px.Bar(x=crypto['date'], y=crypto['volume'], showlegend=False), row=2, col=1)
+
+# Do not show OHLC's rangeslider plot 
+fig.update(layout_xaxis_rangeslider_visible=False)
+fig
+
+
+
+### Tabla datos
+st.dataframe(crypto.style.highlight_max(axis=0))
--- DataTrading.py
+++ DataTrading.py
@@ -1,25 +1,40 @@
 from datetime import datetime
+from operator import index
 import os
 import json
+from pickle import FALSE
 import pandas as pd
 import requests
 import time
 
-    
-endpoint_url = 'https://ftx.com/api/markets'
 
-base_currency = 'ETH'
-quote_currency = 'USD'
-request_url = f'{endpoint_url}/{base_currency}/{quote_currency}'
-daily=str(60*60*24)
-start_date = datetime(2019, 1, 1).timestamp()
-historical = requests.get(
-    f'{request_url}/candles?resolution={daily}&start_time={start_date}'
-).json()
-df = pd.DataFrame(historical['result'])
-df['date'] = pd.to_datetime(
-    df['time']/1000, unit='s', origin='unix'
-) 
-df.drop(['startTime', 'time'], axis=1, inplace=True)
-dataTrading = df
-print(dataTrading)
+
+def getData(crypto, interval, y, mes, dia):   
+    endpoint_url = 'https://ftx.com/api/markets'
+
+    base_currency = crypto
+    quote_currency = 'USD'
+    request_url = f'{endpoint_url}/{base_currency}/{quote_currency}'
+    daily=interval
+    start_date = datetime(y, mes, dia).timestamp()
+    historical = requests.get(
+        f'{request_url}/candles?resolution={daily}&start_time={start_date}'
+    ).json()
+    df = pd.DataFrame(historical['result'])
+    df['date'] = pd.to_datetime(
+        df['time']/1000, unit='s', origin='unix'
+    ) 
+    df.drop(['startTime', 'time'], axis=1, inplace=True)
+    dataTrading = df
+    return dataTrading
+
+
+def getUSD():  
+    d = pd.to_datetime('today').strftime('%d-%m-%Y')
+    url = f'https://mindicador.cl/api/dolar/{d}'
+    response = requests.get(url)
+    data = json.loads(response.text.encode("utf-8"))
+    # Para que el json se vea ordenado, retornar pretty_json
+    pretty_json = json.dumps(data, indent=2)
+    return data
+
